24e89873c91dff84c1e5c8bab5f7bf5700973438
Luke Schlangen <lukeschlangen@gmail.com>
2017-02-21 16:57:54 -0600

    end of lecture

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var songList = [
+  {
+    title: 'We did not start the Phire',
+    artist: 'Billy Joel'
+  },
+  {
+    title: 'Ring of Phire',
+    artist: 'Johnny Cash'
+  }
+];
+
+app.get('/songs', function(req, res) {
+  res.send(songList);
+});
+
+app.post('/newSong', function(req, res){
+  var newSong = req.body;
+  if(newSong.artist !== "Justin Bieber"){
+    songList.push(newSong);
+    console.log(songList);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(500);
+  }
+});
+
+app.listen(3000);
--- public/client.js
+++ public/client.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
   console.log('jquery was correctly sourced!');
+  getAllSongs();
+  function getAllSongs() {
+    $.ajax({
+      type: 'GET',
+      url: '/songs',
+      success: function(response) {
+        console.log('response', response);
+      },
+      error: function(error) {
+        console.log('error', error);
+      }
+    });
+  }
+
+  $('#addSongButton').on('click', function(){
+    var newSongTitle = $('#title').val();
+    var newSongArtist = $('#artist').val();
+    var newSongObject = {
+      title: newSongTitle,
+      artist: newSongArtist
+    };
+    console.log(newSongObject);
+    $.ajax({
+      type: 'POST',
+      url: '/newSong',
+      data: newSongObject,
+      success: function(response) {
+        console.log('response', response);
+        getAllSongs();
+      },
+      error: function(error) {
+        console.log('error', error);
+      }
+    });
+  });
+
 });
