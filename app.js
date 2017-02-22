0cec810447ff569a94991bc5d1b519d7b526d227
samvelie <sam.velie@gmail.com>
2017-02-21 22:15:58 -0600

    adding comments

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'); //this I understand as linking this file to the express files
+var app = express();  //do not understand what we are doing here - is express() an abitrary name or must it be this for express to work?
+var bodyParser = require('body-parser'); //this I see as another link to connect to the body-parser files
 
-app.use(express.static('public'));
+app.use(express.static('public')); //this looks for static files
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); //this is some magic that makes the data work (how would this be done without this - why wouldn't this be a standard feature?)
 
-var songList = [
+var songList = [    //a standard array containing objects
   {
     title: 'We did not start the Phire',
     artist: 'Billy Joel'
@@ -17,19 +17,19 @@ var songList = [
   }
 ];
 
-app.get('/songs', function(req, res) {
-  res.send(songList);
+app.get('/songs', function(req, res) { //this responds to get requests. the parameters must be conventions for the request and response that gets passed
+  res.send(songList); //sending our variable
 });
 
-app.post('/newSong', function(req, res){
-  var newSong = req.body;
+app.post('/newSong', function(req, res){ //I am mostly ok with what this is doing
+  var newSong = req.body;   //would like to investigate this further
   if(newSong.artist !== "Justin Bieber"){
     songList.push(newSong);
     console.log(songList);
-    res.sendStatus(200);
+    res.sendStatus(200); //this is the success response
   } else {
-    res.sendStatus(500);
+    res.sendStatus(500); //this is the error response. I like that we still use standard if else logic gettin to here
   }
 });
 
-app.listen(3000);
+app.listen(3000); //tells our server to listen on port 3000
--- public/client.js
+++ public/client.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
   console.log('jquery was correctly sourced!');
-  getAllSongs();
+  getAllSongs(); //this is the ajax get request functionized
   function getAllSongs() {
     $.ajax({
-      type: 'GET',
-      url: '/songs',
-      success: function(response) {
+      type: 'GET',    //does this have to be capitalized?
+      url: '/songs',  //this is telling the  server what channel it is looking on - is that a good way to put it?
+      success: function(response) {   //response is a naming convention I am guessing?
         console.log('response', response);
       },
-      error: function(error) {
+      error: function(error) {        //did we make an error response possible on the get request?
         console.log('error', error);
       }
     });
   }
 
-  $('#addSongButton').on('click', function(){
+  $('#addSongButton').on('click', function(){ //all this stuff through line 24 is solid JS and jQuery for me
     var newSongTitle = $('#title').val();
     var newSongArtist = $('#artist').val();
     var newSongObject = {
@@ -22,15 +22,15 @@ $(document).ready(function(){
       artist: newSongArtist
     };
     console.log(newSongObject);
-    $.ajax({
+    $.ajax({              //this actually allows us to push data onto the server - this makes me excited!
       type: 'POST',
-      url: '/newSong',
-      data: newSongObject,
-      success: function(response) {
+      url: '/newSong',    //I am unsure why we use another url here - why not the same as the get request? Also why does it not seem like a new page gets made with this[localhost:3000/newSong]?
+      data: newSongObject,  //a bit of magic here for me
+      success: function(response) { //this function is familiar from the weekend
         console.log('response', response);
         getAllSongs();
       },
-      error: function(error) {
+      error: function(error) {    //this is the function that happens upon error response to the ajax post "request" - is request the right word? 
         console.log('error', error);
       }
     });
